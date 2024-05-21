584d4ce12edc6202dfc1a07110035e5cdc43287c
Isidro Paniagua <isidro.paniagua@jamulcasinosd.com>
2024-05-20 22:48:26 -0700

    suped up sidebar css more

--- src/components/Navigation/Sidebar/Sidebar.jsx
+++ src/components/Navigation/Sidebar/Sidebar.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import { useAuth } from '../../AuthContext/AuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faUser, faClockRotateLeft, faTicket, faClone, faPeopleGroup, faDiamond } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname.toLowerCase();
+  console.log(currentPath)
   const { isAuthenticated, login, logout } = useAuth();
     if (!isAuthenticated) {
       return (
@@ -22,11 +25,11 @@ const Sidebar = () => {
   const options = [
     {label: 'Home', link: '/', icon: faHouse},
     {label: 'Account History', link: '/history', icon: faClockRotateLeft },
-    {label: 'Patron', link: '/Patron', icon: faUser },
+    {label: 'Patron', link: '/patron', icon: faUser },
     {label: 'Redeem', link: '/redeem', icon: faTicket},
-    {label: 'Reports', link: '/Reports', icon: faClone},
+    {label: 'Reports', link: '/reports', icon: faClone},
     {label: 'Group Manager', link: '/group-manager', icon: faPeopleGroup },
-    {label: 'Poker', link: '/Poker', icon: faDiamond},
+    {label: 'Poker', link: '/poker', icon: faDiamond},
   ];
 
   return (
@@ -37,7 +40,10 @@ const Sidebar = () => {
               <button
               key={index}
               onClick={() => navigate(option.link)}
-              className={`w-full flex flex-row items-center bg-transparent text-black text-left px-4 gap-2 py-2 rounded-lg font-normal h-[43px]`}
+              className={`
+                ${currentPath === option.link ? 'bg-[#203A45] text-white' : 'hover:bg-[#ececec] hover:text-black text-[#515151]'}
+                w-full flex flex-row items-center text-black text-left rounded-none px-4 gap-2 py-2 font-normal h-[43px]  transition-colors duration-200
+                `}
               >
                 <span className='w-[20px]'>
                   <FontAwesomeIcon icon={option.icon} />
